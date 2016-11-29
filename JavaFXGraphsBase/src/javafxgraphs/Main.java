99053ec05fad4f4795941b017b5011099d6d9215
Agilityforce <rikki.lem@gmail.com>
2016-11-29 02:57:34 +0000

    idk2

--- JavaFXGraphsBase/src/javafxgraphs/Main.java
+++ JavaFXGraphsBase/src/javafxgraphs/Main.java
@@ -75,7 +75,6 @@ public class Main extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("test");
         launch(args);
     }
     
