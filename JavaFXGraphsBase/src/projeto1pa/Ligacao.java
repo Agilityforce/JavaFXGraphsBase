55ac2c1d4163342aa8c75c4deb4517df078cb3a9
Lainfor <rmlainfor@gmail.com>
2016-11-30 13:24:41 +0000

    rip local nesta classe

--- JavaFXGraphsBase/src/projeto1pa/Ligacao.java
+++ JavaFXGraphsBase/src/projeto1pa/Ligacao.java
@@ -15,21 +15,21 @@ public class Ligacao{
     
     private String TipoLigacao;
     private String nomeLigacao;
-    private Local local1;
-    private Local local2;
+    //private Local local1;
+    //private Local local2;
     private int distancia;
     private int preco;
 
-    public Ligacao(String TipoLigacao, String nomeLigacao, Local local1, Local local2, int distancia, int preco) {
+    public Ligacao(String TipoLigacao, String nomeLigacao, int distancia, int preco) {
         this.TipoLigacao = TipoLigacao;
-        this.nomeLigacao = verificarNomeLigacao(local1, local2);        
+        //this.nomeLigacao = verificarNomeLigacao(local1, local2);        
         this.distancia = distancia;
         this.preco = preco;
     }
     
-    private String verificarNomeLigacao(Local local1, Local local2){
-        return local1.toString() + local2.toString();
-    }
+//    private String verificarNomeLigacao(Local local1, Local local2){
+//        return local1.toString() + local2.toString();
+//    }
         
     public String getNomeLigacao(){
         return nomeLigacao;
