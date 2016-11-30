2a852266d1bb63c9ccd0626d8f66f307e36a66fe
Lainfor <rmlainfor@gmail.com>
2016-11-30 14:35:29 +0000

    e o irmao mais velho Kappa

---
+++ JavaFXGraphsBase/src/projeto1pa/CalcularMenosDeslocacoes.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto1pa;
+
+import javafxgraphs.tad.Edge;
+import javafxgraphs.tad.Graph;
+import javafxgraphs.tad.Vertex;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class CalcularMenosDeslocacoes<E, V> implements EstrategiaCalcularSolucao<E, V>{
+    
+    @Override
+    public Iterable<Edge<E, V>> encontrarSolucao(Graph graph, Vertex<V> origem, Vertex<V> destino){
+        if(graph == null || origem == null || destino == null){
+            return null;
+        }
+    }
+}
