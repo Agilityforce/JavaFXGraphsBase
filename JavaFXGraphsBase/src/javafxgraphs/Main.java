357989eb9fb0f64c9e3e2d7520722c16cdf7e193
Agilityforce <rikki.lem@gmail.com>
2016-12-05 00:11:45 +0000

    Teste com vertices ligados em circulo

--- JavaFXGraphsBase/src/javafxgraphs/Main.java
+++ JavaFXGraphsBase/src/javafxgraphs/Main.java
@@ -19,6 +19,7 @@ import javafxgraphs.tad.GraphImpl;
 import javafxgraphs.ui.GraphDraw;
 import javafxgraphs.tad.Vertex;
 import projeto1pa.CalcularMaisBarato;
+import projeto1pa.CalcularMaisCurto;
 import projeto1pa.Ligacao;
 import projeto1pa.Local;
 import projeto1pa.MiniJogoImp;
@@ -33,6 +34,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
        
 //        Implementation of Graph needed!!!
+        /*
         Graph<DummyType, DummyType> myG = new GraphImpl<>(); 
       
         Vertex<DummyType> vA = myG.insertVertex(new DummyType("A"));
@@ -44,21 +46,45 @@ public class Main extends Application {
         myG.insertEdge(vA, vB, new DummyType("AB2")); //second edge
         myG.insertEdge(vA, vC, new DummyType("AC"));
         myG.insertEdge(vA, vD, new DummyType("AD"));
+        */
         
+        /*-------------------------------*/
         
-        MiniJogoImp miniJogo = new MiniJogoImp(new CalcularMaisBarato());
+        Local local1 = new Local("A");
+        Local local2 = new Local("B");     
         
+        MiniJogoImp miniJogo = new MiniJogoImp(new CalcularMaisCurto(), local1, local2);
+        
+        Local local3 = new Local("C");
+        Local local4 = new Local("D");
+        
+        Ligacao ligacao1 = new Ligacao("AB", 10, 10);
+        Ligacao ligacao2 = new Ligacao("BC", 15, 10);
+        Ligacao ligacao3 = new Ligacao("CD", 20, 10);
+        Ligacao ligacao4 = new Ligacao("DA", 25, 10);   
+        
+        miniJogo.insertVertex(local3);
+        miniJogo.insertVertex(local4);
+        
+        miniJogo.insertEdge(local1, local2, ligacao1);
+        miniJogo.insertEdge(local2, local3, ligacao2);
+        miniJogo.insertEdge(local3, local4, ligacao3);
+        miniJogo.insertEdge(local4, local1, ligacao4);
+        
+        /*
         miniJogo.insertVertex(new Local("A"));
         miniJogo.insertVertex(new Local("B"));
         miniJogo.insertVertex(new Local("C"));
         miniJogo.insertVertex(new Local("D"));
+        */
         
-        miniJogo.selecionarOrigemDestino();
-        //miniJogo.calcularSolucao();
+        //miniJogo.selecionarOrigemDestino();
+        System.out.println(miniJogo.calcularSolucao());
         
         System.out.println(miniJogo.getOrigem().element().getNomeLocal());
         System.out.println(miniJogo.getDestino().element().getNomeLocal());
         
+        /*
         //this should be moved to the implementing class
         for (Edge<DummyType, DummyType> edge : myG.edges()) {
             if(edge.element().getId().equalsIgnoreCase("AB")) {
@@ -87,6 +113,7 @@ public class Main extends Application {
         primaryStage.setTitle("Graph!");
         primaryStage.setScene(scene);
         primaryStage.show();
+        */
     }
     
     /**
